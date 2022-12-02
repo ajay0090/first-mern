19c28df0dced5dd7cff71a0f2423941ac1fa3ed1
Ajay Tarole <ajayt@iitbhilai.ac.in>
2022-12-02 08:46:07 +0530

    Update connectDb.js

--- config/connectDb.js
+++ config/connectDb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const connectDb = async () => {
     try {
         console.log("Try to connect mongoose");
-        const conn = await mongoose.connect("mongodb+srv://ajay0090:lBqN292Lqb77amJ5@cluster0.mtoebau.mongodb.net/?retryWrites=true&w=majority");
+        const conn = await mongoose.connect(process.env.DATABASE);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
