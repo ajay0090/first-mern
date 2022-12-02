64c358f2317cf81d8d5d47ba8164b5f726f42a8c
Ajay Tarole <ajayt@iitbhilai.ac.in>
2022-12-02 09:08:21 +0530

    Update connectDb.js

--- config/connectDb.js
+++ config/connectDb.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 const connectDb = async () => {
     try {
         console.log("Try to connect mongoose");
-        const conn = await mongoose.connect(process.env.DATABASE);
+        const conn = await mongoose.connect(process.env.DATABASE, {
+            keepAlive: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+        });
         console.log(`MongoDB Connected: Happy`);
     } catch (error) {
         console.log(error);
